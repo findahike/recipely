e61f6cbacbbd06a127c8cfc491f80b5bcffaf834
Sam Olukotun <otsam88@gmail.com>
2017-05-20 11:44:23 -0700

    Add endpoint to POST a custom recipe

--- recipely-server/server.js
+++ recipely-server/server.js
@@ -23,6 +23,10 @@ app.get('/api/users/recipes', isAuthenticated, handlers.getUsersRecipes);
 
 app.delete('/api/users/recipes/:recipeId', isAuthenticated, handlers.deleteUsersRecipe);
 
+// custom_recipes endpoints
+app.post('/api/users/custom_recipes/', isAuthenticated, handlers.postCustomRecipe);
+
+
 // auth endpoints
 app.post('/api/login', handlers.postLogin);
 
